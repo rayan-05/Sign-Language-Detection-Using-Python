130cde89ab4577de2640b3eaefeab38d64126078
Rayan Mitra <rayan.mitra2021@vitstudent.ac.in>
2024-11-12 09:40:20 +0530

    creating of the model in train classifier file

--- train_classifier.py
+++ train_classifier.py
@@ -3,11 +3,16 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
 import numpy as np
 
 data_dict=pickle.load(open('./data.pickle','rb'))
 
-data = np.asarray(data_dict['data'])
+fixed_length = 50  # Define the target sequence length
+data_padded = [np.pad(item, (0, max(0, fixed_length - len(item))), 'constant')[:fixed_length]
+               for item in data_dict['data']]
+data = np.asarray(data_padded)
+
 labels = np.asarray(data_dict['labels'])
 
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
@@ -18,3 +23,10 @@ model.fit(x_train, y_train)
 
 y_predict = model.predict(x_test)
 
+score = accuracy_score(y_predict, y_test)
+
+print('{}% of samples were classified correctly !'.format(score * 100))
+
+f = open('model.p', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
