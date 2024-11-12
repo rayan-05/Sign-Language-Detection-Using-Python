f68cbed416d9c0333405c6b9f10b2e26bc8771a2
Rayan Mitra <rayan.mitra2021@vitstudent.ac.in>
2024-11-12 09:25:33 +0530

    change in code of create dataset file

--- train_classifier.py
+++ train_classifier.py
@@ -10,3 +10,11 @@ data_dict=pickle.load(open('./data.pickle','rb'))
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
 
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+model = RandomForestClassifier()
+
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
+
